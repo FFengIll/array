748588ac954f1eb3befc9775c1d81552e67948e2
FengYZh <fengyouzheng@gmail.com>
2022-09-11 14:20:52 +0000

    test: add.

---
+++ array_test.go
@@ -0,0 +1,82 @@
+package array
+
+import "testing"
+
+type Sample struct {
+	A string `array:"[0]"`
+	C int `array:"[1]"`
+	B bool `array:"[2]"`
+}
+
+type FailedSample struct {
+	A string `array:"[0]"`
+	B Sample `array:"[2]"`
+}
+
+func TestParse(t *testing.T) {
+	type args struct {
+		d []string
+		v *Sample
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Sample
+		wantErr bool
+	}{
+		{
+			"",
+			args{
+				[]string{"first", "2", "false"},
+				&Sample{},
+			},
+			&Sample{"first",2,false},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Parse(tt.args.d, tt.args.v)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
+			} else if *tt.args.v != *tt.want {
+				t.Errorf("Parse() get = %v, want %v", tt.args.v, tt.want)
+			} else{
+				t.Log(tt.args.v)
+			}
+		})
+	}
+}
+
+func TestParseFailed(t *testing.T) {
+	type args struct {
+		d []string
+		v *FailedSample
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *FailedSample
+		wantErr bool
+	}{
+		{
+			"",
+			args{
+				[]string{"first", "second", "third"},
+				&FailedSample{},
+			},
+			&FailedSample{"first", Sample{}},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := Parse(tt.args.d, tt.args.v)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
+			} else if *tt.args.v != *tt.want {
+				t.Errorf("Parse() get = %v, want %v", tt.args.v, tt.want)
+			}
+		})
+	}
+}
